71be2588ddb6267164b5dcdbfae5585200c15475
Matt Zhang <set.stun@gmail.com>
2017-10-15 18:25:27 -0500

    Button and background animations now all work

--- js/email.js
+++ js/email.js
@@ -1,4 +1,8 @@
 function submit_email() {
-    var emails= firebase.database().ref('emails');
-    emails.push({email: 'new@email.com'})
-});
+    var emails = firebase.database().ref('emails');
+    emails.push({email: document.getElementById('email').value});
+    document.getElementById('submit').innerHTML = 'THANK YOU!';
+    document.getElementById('email').value = 'YOU WILL BE NOTIFIED WHEN WE OPEN!';
+    $(document.getElementById('submit')).attr("disabled", "disabled");
+    document.body.style.background = "#A2C7E5";
+}
